fdb8214bce4a54bffa74da4862a9dd3fdc143716
EmiArn <emily.arnelid@gmail.om>
2021-10-31 16:36:30 +0100

    First commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args){
+        System.out.println("Skriv in månadshyran: ");
+        Scanner input = new Scanner(System.in);
+
+    }
+
+}
---
+++ src/Rented.java
@@ -0,0 +1,6 @@
+public class Rented {
+    private double monthlyRent;
+    public Rented(double inputRent){
+        this.monthlyRent = inputRent;
+    }
+}
