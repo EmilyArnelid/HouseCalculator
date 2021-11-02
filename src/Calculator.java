fc3a855e957dffef49d8b676006ba7c7bb23cadc
EmiArn <emily.arnelid@gmail.om>
2021-11-02 15:11:28 +0100

    add menu

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,36 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args){
-        System.out.println("Skriv in månadshyran: ");
+    private static void createHome() {
+        System.out.println("Vilken typ av boende väljer du? ");
+        System.out.println("1. Hyresrätt");
+        System.out.println("2. Bostadsrätt");
+        System.out.println("3. Äganderätt");
         Scanner input = new Scanner(System.in);
+        int houseTypes = input.nextInt();
+
+        switch (houseTypes) {
+            case 1:
+                System.out.println("1. Hyresrätt");
+                break;
+            case 2:
+                System.out.println("2. Bostadsrätt");
+                break;
+            case 3:
+                System.out.println("3. Äganderätt");
+                break;
+            default:
+                System.out.println("Välj en typ av boende!");
+
+
+        }
+    }
+    public static void main(String[] args){
+        createHome();
+        System.out.print("Skriv in månadshyran: ");
+       // Scanner input = new Scanner(System.in);
+
+       // String monthlyRent = input.nextLine();
 
     }
 
